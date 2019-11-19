8fc44eb5edcd3035b86889123418e80bde8c2bed
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2019-11-19 13:11:21 +0700

    Progress Schedule page, date picker

--- lib/mainPageContents/schedule.dart
+++ lib/mainPageContents/schedule.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 class Schedule extends StatefulWidget {
 
@@ -7,9 +9,72 @@ class Schedule extends StatefulWidget {
 }
 
 class _ScheduleState extends State<Schedule> {
+  DateTime _currentDate = DateTime.now();
+  DateFormat _formatDate = DateFormat("d MMMM yyyy");
+  
+
+  @override
+  void initState() {
+    
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(MediaQuery.of(context).size.width, 70),
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.fromLTRB(20, 9, 20, 9),
+          child: Material(
+            borderRadius: BorderRadius.circular(20),
+            color: Color(0xFFECECEC),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(20),
+              onTap: () async {
+                DateTime pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: _currentDate,
+                  firstDate: DateTime(1996),
+                  lastDate: DateTime(2030)
+                );
+
+                if (pickedDate != null && pickedDate != _currentDate) {
+                  setState(() {
+                    _currentDate = pickedDate;
+                  });
+                }
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      _formatDate.format(_currentDate),
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xFF2F2F2F)
+                      ),
+                    ),
+                    SvgPicture.asset(
+                      'assets/icons/mdi-calendar-month-outline.svg',
+                      semanticsLabel: 'Calendar',
+                      height: 32,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ),
+      ),
       body: Center(
         child: Text('Schedule'),
       ),
